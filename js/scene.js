ef25e37360dbde1d2142bdd403eb48af10791d2f
Timothy Otholt <timotholt@gmail.com>
2025-02-11 10:43:34 -0800

    fixed far clipping plane bug

--- js/scene.js
+++ js/scene.js
@@ -3,7 +3,7 @@ export function createScene(THREE) {
     // Create scene and camera
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0x000000);
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5000);
 
     // Initialize renderers
     const renderer = new THREE.WebGLRenderer({ antialias: true });
